6320bdc051e3924962a5d5a0ffd28b29fabc7088
Georgie <georgie.webb@essensys.tech>
2017-06-12 16:29:57 +0100

    import commands

--- Week1Project/Plot1.R
+++ Week1Project/Plot1.R
@@ -2,6 +2,7 @@
 
 
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-dest <- c(getwd(), "/Week1Project/power.txt")
-dest <- paste(dest[1], dest[2],sep = "")
-power <- download.file(url, dest)
+temp <- tempfile()
+download.file(url,temp)
+powerdata <- read.table(unz(temp, "household_power_consumption.txt"))
+unlink(temp)
