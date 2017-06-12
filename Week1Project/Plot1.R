07e5272ca73ed796d5b124ab91ad963fda9c461b
Georgie <georgie.webb@essensys.tech>
2017-06-12 16:45:08 +0100

    read zip function

--- Week1Project/Plot1.R
+++ Week1Project/Plot1.R
@@ -1,8 +1,11 @@
 
 
 
-url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-temp <- tempfile()
-download.file(url,temp)
-powerdata <- read.table(unz(temp, "household_power_consumption.txt"))
-unlink(temp)
+readZipFile <- function(){
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  temp <- tempfile()
+  download.file(url,temp)
+  powerdata <- read.table(unz(temp, "household_power_consumption.txt"))
+  unlink(temp)
+
+}
