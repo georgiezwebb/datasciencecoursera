c6f576e8ce50489c881d9b59c3273de6909e91d0
Georgie <georgie.webb@essensys.tech>
2017-06-13 15:33:29 +0100

    only data between data range

--- Week1Project/Plot1.R
+++ Week1Project/Plot1.R
@@ -1,11 +1,12 @@
-
+library(lubridate)
+library(dplyr)
 
 
 readZipFile <- function(){
   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   temp <- tempfile()
   download.file(url,temp)
-  powerdata <- read.table(unz(temp, "household_power_consumption.txt"))
+  data <- read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";", na.strings = '?')
   unlink(temp)
-
+  data$Date  <- dmy(data$Date)
 }
